4c364e4d5ba8cca94192ebb85c259009e4a47a55
danielluccio <daniel130877@gmail.com>
2025-09-02 10:24:50 -0300

    Criação de login, register, logout no sistema já funcional

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,9 @@ from . import views
 app_name = "users"
 
 urlpatterns = [
+    path('login_user/', views.login_user, name='login_user'),
+    path('register_user/', views.register_user, name='register_user'),
+    path('logout_user/', views.logout_user, name='logout_user'),
     path('private_page_1/', views.private_page_1, name='private_page_1'),
     path('private_page_2/', views.private_page_2, name='private_page_2')
 ]
